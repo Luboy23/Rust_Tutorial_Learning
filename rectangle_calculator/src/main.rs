a39db2568c51b0b8c0a5ea5997626ea5beed3617
Luboy23 <126079708+Luboy23@users.noreply.github.com>
2024-09-03 02:44:19 +0800

    使用impl方法进行长方形计算

--- rectangle_calculator/src/main.rs
+++ rectangle_calculator/src/main.rs
@@ -1,21 +1,54 @@
-fn main() {
-    let rect = Rectangle{
-       width:30,
-       length:50
-    };
-    println!("面积为：{}", area(&rect));
-    println!("面积为：{:#?}", rect);
-
-
-}
-
-fn area(rect: &Rectangle) -> u32 {
-    return rect.width * rect.length
-}
-
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     length: u32
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.length
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle{
+            width:size,
+            length:size
+        }
+    }
+}
+
+fn main() {
+
+    let s = Rectangle::square(20);
+
+    let rect1 = Rectangle{
+        width:30,
+        length:50
+    };
+
+    let rect2 = Rectangle{
+        width:10,
+        length:40
+    };
+
+    let rect3 = Rectangle{
+        width:30,
+        length:50
+    };
+
+println!("{}", rect1.can_hold(&rect2));
+println!("{}", rect1.can_hold(&rect3));
+
+
+    println!("面积为：{}", rect1.area());
+    println!("面积为：{:#?}", rect1);
+    println!("面积为：{:#?}", s);
+
+}
+
+
+
 
